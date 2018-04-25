9aa951869cc0fbe841006542cceaefaf02edff4a
Chris Aby Antony <antonychrisaby@gmail.com>
2018-04-25 16:35:14 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {LineChart, Tooltip, XAxis, CartesianGrid, Line} from 'recharts';
+import {AreaChart, Area,YAxis } from 'recharts';
+import WordCloud from 'react-d3-cloud';
+import logo from './logo.svg';
+import './App.css';
+const sdata = require("./sdata.json")
+
+class App extends Component {
+  render() {   
+   return (<p>Hello</p>
+//       <Router>
+//       <Route
+//           component={Login}
+//           path="/"
+            
+//       />
+//       <Route
+//           component={About}
+//           path="/about"
+//       />
+//       <Route
+//           component={Contact}
+//           path="/contact"
+//       />
+// </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './footer.css';
+class Footer extends React.Component{
+    render(){
+        return  (<div className="Footer"> </div>)
+    }
+}
+
